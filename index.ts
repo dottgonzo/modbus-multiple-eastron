3a9c66dba1b95d43cf98e0aa980094f7d95a4c01
dario <darioyzf@gmail.com>
2016-08-29 14:00:29 +0200

    multiple with loop and timecheck

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import * as Promise from 'bluebird';
 import * as async from 'async';
 import ModEastron from "modbus-eastron"
+import afterSync from "aftertimesync"
+
 
 
 interface EastronDevice {
@@ -15,11 +17,107 @@ interface EastronDevice {
 }
 
 
-export default function ModMultiEastron(devices: EastronDevice[]) {
+export default class ModEastronKernel {
+    devices: EastronDevice[]
+    activeloop: any;
 
-    return new Promise((resolve, reject) => {
 
-        if (devices && devices.length && devices[0] && devices[0].id) {
+    constructor() {
+        this.devices = []
+    }
+
+    addDevices(devices: EastronDevice[]) {
+        if (!devices || !devices.length || !devices[0].id) {
+            throw Error('wrong devices config for kernel modbus eastron')
+        } else {
+
+            for (let i = 0; i < devices.length; i++) {
+                this.addDevice(devices[i])
+            }
+
+        }
+
+    }
+
+    addDevice(device: EastronDevice) {
+        if (!device || !device.id) {
+            throw Error('wrong device conf for kernel modbus eastron')
+        } else {
+            this.devices.push(device)
+        }
+
+    }
+
+
+    loop(callback: Function, o?: { interval: number, checkTime: boolean }) {
+
+        const _this = this;
+        if (!_this.activeloop) {
+
+
+            function looping() {
+                let interval = 300000
+                if (o && o.interval) interval = o.interval
+
+
+                function cycle() {
+                    _this.activeloop = setInterval(function () {
+                        this.data().then((a) => {
+                            callback(a)
+                        }).catch((err) => {
+                            console.error(err)
+                        })
+                    }, interval)
+                }
+
+
+
+
+                this.data().then((a) => {
+                    callback(a)
+                    cycle()
+                }).catch((err) => {
+                    console.error(err)
+                    cycle()
+                })
+
+
+            }
+
+            if (o && o.checkTime) {
+                afterSync().then(() => {
+                    looping()
+                }).catch((err) => {
+                    console.error(err)
+                    throw Error(err)
+                })
+            } else {
+                looping()
+            }
+
+
+
+        } else {
+            throw Error('you must close the actual loop before start another')
+        }
+
+    }
+
+    loopStop() {
+
+        const _this = this;
+        if (_this.activeloop) {
+            clearInterval(_this.activeloop)
+            return 'ok'
+        } else {
+            return 'no loop are running'
+        }
+
+    }
+
+    data() {
+        const devices = this.devices
+        return new Promise((resolve, reject) => {
 
             const answer = [];
 
@@ -42,10 +140,8 @@ export default function ModMultiEastron(devices: EastronDevice[]) {
 
             })
 
-        } else {
-            reject('wrong data conf for modbus multiple eastron')
-        }
+        })
+    }
 
-    })
 
 }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/superagent/index.d.ts" />
